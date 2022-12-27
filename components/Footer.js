8c5347299d853f7907a8f3647773ed94bb0edd2e
kemcet <kemalcetmensm@gmail.com>
2022-12-27 14:21:09 +0300

    adding github to footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,10 +4,17 @@ import { BsFacebook } from 'react-icons/bs';
 import { BsYoutube } from 'react-icons/bs';
 import { AiFillTwitterCircle } from 'react-icons/ai';
 import { AiFillInstagram } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className={ styles.footer }>
+      <div className={ styles.git }>
+        <Link href="https://github.com/kemalcetmen">
+          <AiFillGithub />
+        </Link>
+      </div>
       <div title="we are not going anywhere" className={ styles.fb }>
         <BsFacebook />
       </div>
